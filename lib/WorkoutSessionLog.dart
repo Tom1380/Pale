dbbc42ee5fa62e6a9763213724394da50ce1adbe
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2020-07-10 22:45:17 +0200

    Split code in 2 files.

---
+++ lib/WorkoutSessionLog.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+class WorkoutSessionLog extends StatefulWidget {
+  WorkoutSessionLog({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _WorkoutSessionLogState createState() => _WorkoutSessionLogState();
+}
+
+class _WorkoutSessionLogState extends State<WorkoutSessionLog> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (BuildContext context, int index) {
+              return WorkoutSessionPreview();
+            },
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Nuova sessione',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class WorkoutSessionPreview extends StatefulWidget {
+  @override
+  _WorkoutSessionPreviewState createState() => _WorkoutSessionPreviewState();
+}
+
+class _WorkoutSessionPreviewState extends State<WorkoutSessionPreview> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Card(
+        elevation: 7,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      'A',
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border(
+                        left: BorderSide(
+                          width: 1,
+                          color: Colors.grey[300],
+                        ),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        WorkoutSessionPreviewHighlightsText('Highlights'),
+                        WorkoutSessionPreviewHighlightsText('Bench 80kg x 3'),
+                        WorkoutSessionPreviewHighlightsText('Squat 105kg x 5'),
+                        WorkoutSessionPreviewHighlightsText(
+                          'Trap Bar DL 130x5',
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Ieri - 07/07/20 10:47',
+                  style: TextStyle(
+                    color: Colors.blueGrey[600],
+                  ),
+                ),
+              )
+            ],
+            crossAxisAlignment: CrossAxisAlignment.start,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WorkoutSessionPreviewHighlightsText extends StatelessWidget {
+  String _string;
+  WorkoutSessionPreviewHighlightsText(this._string);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: 8,
+      ),
+      child: Text(
+        this._string,
+        style: TextStyle(
+          color: Colors.blueGrey[600],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'WorkoutSessionLog.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -15,126 +17,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         appBarTheme: AppBarTheme(),
       ),
-      home: MyHomePage(title: 'Sessioni'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Container(
-          child: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (BuildContext context, int index) {
-              return WorkoutSessionPreview();
-            },
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Nuova sessione',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class WorkoutSessionPreview extends StatefulWidget {
-  @override
-  _WorkoutSessionPreviewState createState() => _WorkoutSessionPreviewState();
-}
-
-class _WorkoutSessionPreviewState extends State<WorkoutSessionPreview> {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Card(
-        elevation: 10,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'A',
-                      style: TextStyle(
-                        fontSize: 40,
-                        color: Colors.redAccent,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border(
-                        left: BorderSide(
-                          width: 1,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        WorkoutSessionPreviewHighlightsText('Highlights'),
-                        WorkoutSessionPreviewHighlightsText('Bench 80kg x 3'),
-                        WorkoutSessionPreviewHighlightsText('Squat 105kg x 5'),
-                        WorkoutSessionPreviewHighlightsText(
-                            'Trap Bar DL 130x5'),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Yesterday - 07/07/20 10:47',
-                  style: TextStyle(
-                    color: Colors.blueGrey[600],
-                  ),
-                ),
-              )
-            ],
-            crossAxisAlignment: CrossAxisAlignment.start,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class WorkoutSessionPreviewHighlightsText extends StatelessWidget {
-  String _string;
-  WorkoutSessionPreviewHighlightsText(this._string);
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        left: 8, //4,
-      ),
-      child: Text(
-        this._string,
-        style: TextStyle(
-          color: Colors.blueGrey[600],
-        ),
-      ),
+      home: WorkoutSessionLog(title: 'Sessioni'),
     );
   }
 }
