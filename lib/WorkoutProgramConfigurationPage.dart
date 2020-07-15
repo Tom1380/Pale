4fca24555a913a4b4e0223a2baa72f3cc2e6960f
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2020-07-16 00:40:04 +0200

    Worked on the workout program configurator.

--- lib/WorkoutProgramConfigurationPage.dart
+++ lib/WorkoutProgramConfigurationPage.dart
@@ -16,18 +16,161 @@ class _WorkoutProgramConfigurationState
         title: Text('La mia scheda'),
       ),
       body: ListView.builder(
-        itemCount: 20,
+        itemCount: 3,
         itemBuilder: (BuildContext context, int index) {
-          return WorkoutProgramDayConfigurator();
+          return WorkoutProgramDayPreview();
         },
       ),
     );
   }
 }
 
-class WorkoutProgramDayConfigurator extends StatefulWidget {
+class WorkoutProgramDayPreview extends StatefulWidget {
   TextEditingController controller = new TextEditingController();
+  @override
+  _WorkoutProgramDayPreviewState createState() =>
+      _WorkoutProgramDayPreviewState();
+}
 
+class _WorkoutProgramDayPreviewState extends State<WorkoutProgramDayPreview> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: GestureDetector(
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(16, 16, 0, 16),
+                      child: Text(
+                        'A',
+                        style: TextStyle(
+                          fontSize: 40,
+                          color: Theme.of(context).accentColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    WorkoutProgramDayExerciseList([
+                      'Trazioni 5 x max',
+                      'Panca piana 10-7-5-3',
+                      'T-Bar row 10-7-5',
+                      'Military press 3 x 10',
+                      'Squat 4 x 5',
+                      'Curl 4 x 6-8',
+                      'French press 4 x 6-8',
+                      'Crunch inversi a terra 4 x 15',
+                    ]),
+                  ],
+                ),
+              ],
+              crossAxisAlignment: CrossAxisAlignment.start,
+            ),
+          ),
+        ),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => WorkoutProgramDayConfigurator(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class WorkoutProgramDayExerciseList extends StatelessWidget {
+  final List<String> exerciseList;
+  WorkoutProgramDayExerciseList(this.exerciseList);
+  Widget rowChild(BuildContext context, List<String> strings) {
+    List<Text> texts = [];
+    for (int i = 0; i < strings.length; ++i) {
+      texts.add(
+        Text(
+          strings[i],
+          overflow: TextOverflow.fade,
+          style: TextStyle(
+            fontSize: 8,
+            color: Colors.blueGrey[600],
+          ),
+        ),
+      );
+    }
+    return Container(
+      margin: EdgeInsets.only(
+        left: 16,
+      ),
+      padding: EdgeInsets.only(
+        left: 16,
+      ),
+      decoration: BoxDecoration(
+        border: Border(
+          left: BorderSide(
+            width: 1,
+            color: Colors.grey[300],
+          ),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: texts,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> rowChildren = [];
+    if (this.exerciseList.length <= 4) {
+      rowChildren.add(
+        rowChild(
+          context,
+          this.exerciseList,
+        ),
+      );
+    } else if (this.exerciseList.length <= 8) {
+      rowChildren.add(
+        rowChild(
+          context,
+          this.exerciseList.sublist(0, 4),
+        ),
+      );
+      rowChildren.add(
+        rowChild(
+          context,
+          this.exerciseList.sublist(4, this.exerciseList.length),
+        ),
+      );
+    } else {
+      int middle = (this.exerciseList.length / 2).round();
+      rowChildren.add(
+        rowChild(
+          context,
+          this.exerciseList.sublist(0, middle),
+        ),
+      );
+      rowChildren.add(
+        rowChild(
+          context,
+          this.exerciseList.sublist(middle, this.exerciseList.length),
+        ),
+      );
+    }
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: rowChildren,
+    );
+  }
+}
+
+class WorkoutProgramDayConfigurator extends StatefulWidget {
   @override
   _WorkoutProgramDayConfiguratorState createState() =>
       _WorkoutProgramDayConfiguratorState();
@@ -37,52 +180,8 @@ class _WorkoutProgramDayConfiguratorState
     extends State<WorkoutProgramDayConfigurator> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'A',
-                      style: TextStyle(
-                        fontSize: 40,
-                        color: Theme.of(context).accentColor,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(
-                      left: 16,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border(
-                        left: BorderSide(
-                          width: 1,
-                          color: Colors.grey[300],
-                        ),
-                      ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Segnaposto'),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ],
-            crossAxisAlignment: CrossAxisAlignment.start,
-          ),
-        ),
-      ),
+    return Scaffold(
+      appBar: AppBar(title: Text('Giorno A')),
     );
   }
 }
