8d9d7519f48ddc30c4ab9896d2f03e04aeaea230
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2020-07-23 03:25:14 +0200

    Renamed the app from 'pale' to 'Palestra'.

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pale',
+      title: 'Palestra',
       theme: ThemeData(
         primarySwatch: Colors.red,
         accentColor: Colors.red[600],
