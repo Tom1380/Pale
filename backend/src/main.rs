6d39770485d2f58444371f009f5d4a60246df8d6
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2021-05-24 18:56:32 +0200

    Working on a small backend prototype.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,26 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+extern crate serde;
+
+use rocket_contrib::json::Json;
+
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![exercises])
+    .launch();
+}
+
+#[get("/exercises/<query>")]
+fn exercises(query: String) -> Json<Vec<String>> {
+    Json(exercises_search(query))
+}
+
+
+fn exercises_search(query: String) -> Vec<String> {
+    let query = query.to_lowercase();
+    let all = vec!["Bench Press", "Archer Chin-ups", "Squat"];
+
+    all.into_iter().filter(|s| s.to_lowercase().starts_with(&query)).map(|s| s.to_string()).collect()
 }
