bff9eb47b2c35351de8ff4e352c138926bdf811c
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2021-05-30 21:28:24 +0200

    Implemented fuzzy searching for the exercises.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,6 +3,8 @@
 #[macro_use]
 extern crate rocket;
 extern crate serde;
+use fuzzy_matcher::skim::SkimMatcherV2;
+use fuzzy_matcher::FuzzyMatcher;
 
 use rocket_contrib::json::Json;
 
@@ -15,16 +17,19 @@ fn exercises(query: String) -> Json<Vec<String>> {
     Json(exercises_search(query))
 }
 
+// Get all exercises that match fuzzily with the query, sort them by similarity to the query.
 fn exercises_search(query: String) -> Vec<String> {
-    let query = without_spaces(&query).to_lowercase();
+    let matcher = SkimMatcherV2::default();
     let all = vec!["Bench Press", "Archer Chin-ups", "Squat"];
 
-    all.into_iter()
-        .filter(|s| without_spaces(s).to_lowercase().starts_with(&query))
-        .map(|s| s.to_string())
+    let mut v: Vec<_> = all
+        .into_iter()
+        .map(|exercise| (exercise.to_string(), matcher.fuzzy_match(exercise, &query)))
+        .filter(|(_exercise, match_rating)| match_rating.is_some())
+        .collect();
+    v.sort_by(|(_, rating1), (_, rating2)| rating1.cmp(rating2));
+    v.reverse();
+    v.into_iter()
+        .map(|(exercise, _match_rating)| exercise)
         .collect()
-}
-
-fn without_spaces(s: &str) -> String {
-    s.chars().filter(|c| *c != ' ' && *c != '-').collect()
 }
