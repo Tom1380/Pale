7552cdb199cf880c71d5e3a1e0ea894ad2ce0b3e
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2021-05-30 21:09:20 +0200

    Working on bettering the exercise search.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,10 +6,8 @@ extern crate serde;
 
 use rocket_contrib::json::Json;
 
-
 fn main() {
-    rocket::ignite().mount("/", routes![exercises])
-    .launch();
+    rocket::ignite().mount("/", routes![exercises]).launch();
 }
 
 #[get("/exercises/<query>")]
@@ -17,10 +15,16 @@ fn exercises(query: String) -> Json<Vec<String>> {
     Json(exercises_search(query))
 }
 
-
 fn exercises_search(query: String) -> Vec<String> {
-    let query = query.to_lowercase();
+    let query = without_spaces(&query).to_lowercase();
     let all = vec!["Bench Press", "Archer Chin-ups", "Squat"];
 
-    all.into_iter().filter(|s| s.to_lowercase().starts_with(&query)).map(|s| s.to_string()).collect()
+    all.into_iter()
+        .filter(|s| without_spaces(s).to_lowercase().starts_with(&query))
+        .map(|s| s.to_string())
+        .collect()
+}
+
+fn without_spaces(s: &str) -> String {
+    s.chars().filter(|c| *c != ' ' && *c != '-').collect()
 }
