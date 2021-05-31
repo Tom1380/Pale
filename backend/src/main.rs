bf2bcb53fe7edde958ea9054264067a770c39a3c
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2021-05-31 17:21:13 +0200

    The matching indices are now returned too, but this might be reverted.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,20 +5,20 @@ extern crate rocket;
 extern crate serde;
 use fuzzy_matcher::skim::SkimMatcherV2;
 use fuzzy_matcher::FuzzyMatcher;
-
 use rocket_contrib::json::Json;
+use serde::Serialize;
 
 fn main() {
     rocket::ignite().mount("/", routes![exercises]).launch();
 }
 
 #[get("/exercises/<query>")]
-fn exercises(query: String) -> Json<Vec<String>> {
+fn exercises(query: String) -> Json<Vec<ExerciseSearchFuzzyMatch>> {
     Json(exercises_search(query))
 }
 
 // Get all exercises that match fuzzily with the query, sort them by similarity to the query.
-fn exercises_search(query: String) -> Vec<String> {
+fn exercises_search(query: String) -> Vec<ExerciseSearchFuzzyMatch> {
     let query = query.to_lowercase();
     let matcher = SkimMatcherV2::default();
     let all = vec![
@@ -29,18 +29,30 @@ fn exercises_search(query: String) -> Vec<String> {
         "Overhead Press",
         "Hip Thrust",
         "Trap Bar Deadlift",
-        "Pendlay Row"
+        "Pendlay Row",
     ];
 
     let mut v: Vec<_> = all
         .into_iter()
-        .map(|exercise| (exercise, matcher.fuzzy_match(exercise, &query)))
-        .filter(|(_exercise, match_rating)| match_rating.is_some())
+        .map(|exercise| (exercise, matcher.fuzzy_indices(exercise, &query)))
+        .filter(|(_exercise, match_results)| match_results.is_some())
+        .map(|(exercise, match_results)| (exercise, match_results.unwrap()))
         .collect();
     v.sort_by(|(_, rating1), (_, rating2)| rating1.cmp(rating2));
     v.reverse();
     v.into_iter()
-        .map(|(exercise, _match_rating)| exercise.to_string())
+        .map(
+            |(exercise, (_match_rating, match_indices))| ExerciseSearchFuzzyMatch {
+                exercise: exercise.to_string(),
+                indices: match_indices,
+            },
+        )
         .take(20)
         .collect()
+}
+
+#[derive(Serialize)]
+struct ExerciseSearchFuzzyMatch {
+    exercise: String,
+    indices: Vec<usize>,
 }
