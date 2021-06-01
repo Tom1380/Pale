5b295b1985a2db9e22fd0a9dd4866ff3f9d821e0
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2021-06-02 01:55:46 +0200

    The exercise's name column is now not null.

--- backend/initial_schema.sql
+++ backend/initial_schema.sql
@@ -2,7 +2,7 @@ CREATE TYPE EXERCISE_TYPE AS ENUM ('reps', 'isometric');
 
 CREATE TABLE exercises (
 	id SERIAL,
-	name TEXT UNIQUE,
+	name TEXT UNIQUE NOT NULL,
 	type EXERCISE_TYPE,
 	PRIMARY KEY (id)
 );
