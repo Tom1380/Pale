dd840bd3a8d1fd133a25920e594e67e22dd2c548
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2021-06-19 16:12:07 +0200

    Added more tables, used to store training programs.

--- backend/initial_schema.sql
+++ backend/initial_schema.sql
@@ -3,6 +3,28 @@ CREATE TYPE EXERCISE_TYPE AS ENUM ('reps', 'isometric');
 CREATE TABLE exercises (
 	id SERIAL,
 	name TEXT UNIQUE NOT NULL,
-	type EXERCISE_TYPE,
+	type EXERCISE_TYPE NOT NULL,
+	notes TEXT,
 	PRIMARY KEY (id)
+);
+
+CREATE TABLE training_programs (
+	id SERIAL,
+	name TEXT UNIQUE NOT NULL,
+	notes TEXT,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE training_programs_exercises (
+	training_program_id INTEGER NOT NULL REFERENCES training_programs (id),
+	-- Could be 'A', or maybe 'Push', or even 'Back day'.
+	day_name TEXT NOT NULL,
+	exercise_id INTEGER NOT NULL REFERENCES exercises (id),
+	sets INTEGER,
+	reps INTEGER,
+	-- If 0, MAX
+	seconds INTEGER,
+	-- If 0, MAX
+	notes TEXT,
+	PRIMARY KEY (training_program_id, day_name)
 );
