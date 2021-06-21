c8abdecb7417a90bd19641e3627dd507445ab4d8
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2021-06-21 16:24:30 +0200

    Separated training program days from the actual exercises in the database.

--- backend/initial_schema.sql
+++ backend/initial_schema.sql
@@ -15,11 +15,17 @@ CREATE TABLE training_programs (
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE training_programs_exercises (
+CREATE TABLE training_programs_days (
 	id SERIAL,
 	training_program_id INTEGER NOT NULL REFERENCES training_programs (id),
+	name TEXT NOT NULL UNIQUE,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE training_programs_days_exercises (
+	id SERIAL,
+	training_program_day_id INTEGER NOT NULL REFERENCES training_programs_days (id),
 	-- Could be 'A', or maybe 'Push', or even 'Back day'.
-	day_name TEXT NOT NULL,
 	exercise_id INTEGER NOT NULL REFERENCES exercises (id),
 	sets INTEGER,
 	reps INTEGER,
