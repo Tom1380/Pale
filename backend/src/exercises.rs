7215470e326269c5efd34b6977f8b778aaa7b8f6
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2021-06-29 20:45:56 +0200

    Ran cargo fmt.

--- backend/src/exercises.rs
+++ backend/src/exercises.rs
@@ -4,24 +4,24 @@ use postgres_types::{FromSql, ToSql};
 #[derive(Debug, ToSql, FromSql)]
 #[postgres(name = "exercise_type")]
 pub enum ExerciseType {
-	#[postgres(name = "reps")]
-	Reps,
-	#[postgres(name = "isometric")]
-	Isometric,
+    #[postgres(name = "reps")]
+    Reps,
+    #[postgres(name = "isometric")]
+    Isometric,
 }
 
 // TODO use a request guard for the exercise type.
 #[post("/exercise/add/<name>/<exercise_type>")]
 pub fn add_exercise(name: String, exercise_type: String) {
-	let exercise_type = match exercise_type.as_str() {
-		"reps" => ExerciseType::Reps,
-		"isometric" => ExerciseType::Isometric,
-		_ => panic!(),
-	};
-	let mut conn = new_conn().unwrap();
-	conn.execute(
-		"INSERT INTO exercises (name, type) VALUES ($1, $2)",
-		&[&name, &exercise_type],
-	)
-	.unwrap();
+    let exercise_type = match exercise_type.as_str() {
+        "reps" => ExerciseType::Reps,
+        "isometric" => ExerciseType::Isometric,
+        _ => panic!(),
+    };
+    let mut conn = new_conn().unwrap();
+    conn.execute(
+        "INSERT INTO exercises (name, type) VALUES ($1, $2)",
+        &[&name, &exercise_type],
+    )
+    .unwrap();
 }
