0991a6220b2b8cb37e2d4c188ca424b6522cef61
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2021-07-15 22:38:19 +0200

    The endpoint to register new exercises now returns the id.

--- backend/src/exercises.rs
+++ backend/src/exercises.rs
@@ -1,5 +1,7 @@
 use crate::db::new_conn;
 use postgres_types::{FromSql, ToSql};
+use rocket_contrib::json::Json;
+use serde::Serialize;
 
 #[derive(Debug, ToSql, FromSql)]
 #[postgres(name = "exercise_type")]
@@ -12,16 +14,27 @@ pub enum ExerciseType {
 
 // TODO use a request guard for the exercise type.
 #[post("/exercise/add/<name>/<exercise_type>")]
-pub fn add_exercise(name: String, exercise_type: String) {
+pub fn add_exercise(name: String, exercise_type: String) -> Json<ReturnedId> {
     let exercise_type = match exercise_type.as_str() {
         "reps" => ExerciseType::Reps,
         "isometric" => ExerciseType::Isometric,
         _ => panic!(),
     };
     let mut conn = new_conn().unwrap();
-    conn.execute(
-        "INSERT INTO exercises (name, type) VALUES ($1, $2)",
-        &[&name, &exercise_type],
-    )
-    .unwrap();
+    Json(ReturnedId {
+        id: conn
+            .query(
+                "INSERT INTO exercises (name, type) VALUES ($1, $2) RETURNING id",
+                &[&name, &exercise_type],
+            )
+            .unwrap()
+            .get(0)
+            .unwrap()
+            .get(0),
+    })
+}
+
+#[derive(Serialize)]
+pub struct ReturnedId {
+    id: i32,
 }
